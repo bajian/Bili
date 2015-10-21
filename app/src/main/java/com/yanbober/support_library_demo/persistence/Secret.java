ed3f4d2547d1bbbaf1293ca4dfde78ffee082aca
bajian <313066164@qq.com>
2015-10-21 10:02:40 +0800

    Update Secret.java

--- app/src/main/java/com/yanbober/support_library_demo/persistence/Secret.java
+++ app/src/main/java/com/yanbober/support_library_demo/persistence/Secret.java
@@ -4,6 +4,6 @@ package com.yanbober.support_library_demo.persistence;
  * Created by Administrator on 2015/10/14.
  */
 public class Secret {
-    public static String App_Key="85eb6835b0a1034e";
-    public static String App_Secret="2ad42749773c441109bdc0191257a664";
+    public static String App_Key="";
+    public static String App_Secret="";
 }

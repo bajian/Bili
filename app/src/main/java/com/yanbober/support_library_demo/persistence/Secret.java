15b308375d248cc33b05fd6edd3ded407d325956
bajian <313066164@qq.com>
2015-10-21 10:09:20 +0800

    Delete Secret.java

--- app/src/main/java/com/yanbober/support_library_demo/persistence/Secret.java
+++
@@ -1,9 +0,0 @@
-package com.yanbober.support_library_demo.persistence;
-
-/**
- * Created by Administrator on 2015/10/14.
- */
-public class Secret {
-    public static String App_Key="";
-    public static String App_Secret="";
-}
